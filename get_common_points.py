b4afc2b863e7a0889454a15bf7c50f8704190a6a
Gobidev <adrian.groh@t-online.de>
2019-11-07 17:04:20 +0100

    Update get_common_points.py

--- get_common_points.py
+++ get_common_points.py
@@ -1,12 +1,11 @@
 from sympy import *
-f_function = ""
 x = Symbol("x")
 a = Symbol("a")
 b = Symbol("b")
 
 
-def f_x(x):
-    return eval(f_function)
+def f_x(x, p_function):
+    return eval(p_function)
 
 
 def get_common_points(p_function):
@@ -15,7 +14,7 @@ def get_common_points(p_function):
     common_x_values = solve(eval(function_a) - eval(function_b), x)
     results = []
     for value in common_x_values:
-        results.append((value, f_x(value)))
+        results.append((value, f_x(value, p_function)))
     return results
 
 
